e0f7fffa76d19eb5adb430712faaf0026c026289
Dhruv Makwana <dhruv.makwana.169@gmail.com>
2024-01-01 19:31:02 -0500

    Initial commit

---
+++ backend/chat/models.py
@@ -0,0 +1,2 @@
+from django.db import models
+from mongoengine import Document, StringField, ListField, ReferenceField, EmbeddedDocument, EmbeddedDocumentField
---
+++ backend/chat/serializers.py
@@ -0,0 +1,2 @@
+from rest_framework_mongoengine import serializers
+from .models import *
