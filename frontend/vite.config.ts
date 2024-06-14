d0a9908b8b763fe5f894420ff4be29d7391d4da2
Dhruv Makwana <dhruv.makwana.169@gmail.com>
2024-06-14 10:26:49 -0400

    test: try proxy for the testing branch

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,13 +10,19 @@ export default defineConfig({
   server: {
     host: "localhost",
     port: 3000,
-    // proxy: {
-    //   '/cypher-talk': {
-    //     target: 'https://20.96.176.239',
-    //     changeOrigin: true,
-    //     // secure: false,
-    //     rewrite: (path) => path.replace(/^\/cypher-talk/, '/cypher-talk'),
-    //   },
-    // },
+    proxy: {
+      '/cypher-talk': {
+        target: 'https://20.96.176.239',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/cypher-talk/, '/cypher-talk'),
+      },
+    },
+    // headers: {
+    //   'Access-Control-Allow-Credentials': 'true',
+    //   'Access-Control-Allow-Origin': '*',
+    //   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
+    //   'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    // }
   }
 });
