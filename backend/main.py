e218f4e5cb639fb3adb41f99a9bbb4214c461209
Dhruv Makwana <dhruv.makwana.169@gmail.com>
2024-09-28 16:17:46 -0400

    Backend debug start

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
