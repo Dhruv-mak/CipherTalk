7ec67bce5c9f174cefbc6e44ebc9112aefc10e5d
Dhruv Makwana <dhruv.makwana.169@gmail.com>
2025-01-04 12:13:07 -0500

    check if build works

--- frontend/src/pages/chat.tsx
+++ frontend/src/pages/chat.tsx
@@ -51,7 +51,7 @@ const ChatPage = () => {
   const currentChat = useRef<ChatListItemInterface | null>(null);
 
   // To keep track of the setTimeout function
-  const typingTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const typingTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
   // Define state variables and their initial values using 'useState'
   const [isConnected, setIsConnected] = useState(false); // For tracking socket connection
